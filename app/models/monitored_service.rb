72b900eb88fe204703a6a2cafc050ccd0a8be7e0
Egor Khanko <egorkhanko@gmail.com>
2022-06-01 00:43:06 +0300

    Add annotate gem for annotating models

--- app/models/monitored_service.rb
+++ app/models/monitored_service.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: monitored_services
+#
+#  id         :bigint           not null, primary key
+#  url        :string           not null
+#  schedule   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class MonitoredService < ApplicationRecord
   has_and_belongs_to_many :recipients
 end
--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: recipients
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Recipient < ApplicationRecord
   has_and_belongs_to_many :monitored_services
 end
