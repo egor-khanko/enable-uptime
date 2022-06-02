43aa24beefb0b705914938818342d466bd985bec
Egor Khanko <egorkhanko@gmail.com>
2022-06-02 12:05:39 +0300

    Remove default ENV vars from cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,9 +5,6 @@ require File.expand_path('../environment', __FILE__) # requiring rails applicati
 set :output, 'log/cron.log' unless Rails.env.production?
 set :environment, Rails.env
 
-env 'ENABLE_UPTIME_DATABASE_USERNAME', 'postgres'
-env 'ENABLE_UPTIME_DATABASE_PASSWORD', ''
-
 MonitoredService.all.each do |monitored_service|
   every monitored_service.schedule do
     runner "ServiceAvaliabilityCheck.perform_later(#{monitored_service.id})"
